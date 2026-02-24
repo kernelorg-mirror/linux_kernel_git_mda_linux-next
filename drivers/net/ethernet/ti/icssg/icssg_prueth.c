36ab3eff1684ec9a57e228d7415d7f58aa88e405
MD Danish Anwar <danishanwar@ti.com>
2026-02-24 18:18:02 +0530

    net: ti: icssg-prueth: Add Frame Preemption MAC Merge support
    This patch introduces qos support for the icssg driver. This
    includes adding support to configure mqprio qdisc and IET FPE.
    By default all the queues are marked as express which can be
    overwritten by the mqprio tc mask passed by tc qdisc.
    
    icssg_config_ietfpe() work thread takes care of configuring
    IET FPE in the firmware and triggering the verify state machine
    based on the MAC Merge sublayer parameters set by the ethtool.
    The firmware handles the cleanup after successful mac verification.
    And in case the remote peer fails to respond to verify command
    before the timeout (5secs), then FPE is disabled by firmware.
    
    During link up/down, verify state machine gets triggered again
    based on the state of fpe_enabled and fpe_active.
    
    Signed-off-by: Meghana Malladi <m-malladi@ti.com>
    Signed-off-by: MD Danish Anwar <danishanwar@ti.com>

--- drivers/net/ethernet/ti/icssg/icssg_prueth.c
+++ drivers/net/ethernet/ti/icssg/icssg_prueth.c
@@ -388,9 +388,11 @@ static void emac_adjust_link(struct net_device *ndev)
 			spin_unlock_irqrestore(&emac->lock, flags);
 			icssg_config_set_speed(emac);
 			icssg_set_port_state(emac, ICSSG_EMAC_PORT_FORWARD);
+			icssg_qos_link_up(ndev);
 
 		} else {
 			icssg_set_port_state(emac, ICSSG_EMAC_PORT_DISABLE);
+			icssg_qos_link_down(ndev);
 		}
 	}
 
@@ -1152,6 +1154,7 @@ static int emac_ndo_stop(struct net_device *ndev)
 	prueth_destroy_rxq(emac);
 
 	cancel_work_sync(&emac->rx_mode_work);
+	cancel_work_sync(&emac->qos.iet.fpe_config_task);
 
 	/* Destroying the queued work in ndo_stop() */
 	cancel_delayed_work_sync(&emac->stats_work);
@@ -1652,6 +1655,7 @@ static const struct net_device_ops emac_netdev_ops = {
 	.ndo_hwtstamp_get = icssg_ndo_get_ts_config,
 	.ndo_hwtstamp_set = icssg_ndo_set_ts_config,
 	.ndo_xsk_wakeup = prueth_xsk_wakeup,
+	.ndo_setup_tc = icssg_qos_ndo_setup_tc,
 };
 
 static int prueth_netdev_init(struct prueth *prueth,
@@ -1685,6 +1689,8 @@ static int prueth_netdev_init(struct prueth *prueth,
 	INIT_WORK(&emac->rx_mode_work, emac_ndo_set_rx_mode_work);
 
 	INIT_DELAYED_WORK(&emac->stats_work, icssg_stats_work_handler);
+
+	icssg_qos_init(ndev);
 
 	ret = pruss_request_mem_region(prueth->pruss,
 				       port == PRUETH_PORT_MII0 ?
@@ -2472,6 +2478,8 @@ netdev_unregister:
 		}
 		unregister_netdev(prueth->registered_netdevs[i]);
 		disable_work_sync(&prueth->emac[i]->rx_mode_work);
+		disable_work_sync(&prueth->emac[i]->qos.iet.fpe_config_task);
+		mutex_destroy(&prueth->emac[i]->qos.iet.fpe_lock);
 	}
 
 netdev_exit:
@@ -2533,6 +2541,8 @@ static void prueth_remove(struct platform_device *pdev)
 		prueth->emac[i]->ndev->phydev = NULL;
 		unregister_netdev(prueth->registered_netdevs[i]);
 		disable_work_sync(&prueth->emac[i]->rx_mode_work);
+		disable_work_sync(&prueth->emac[i]->qos.iet.fpe_config_task);
+		mutex_destroy(&prueth->emac[i]->qos.iet.fpe_lock);
 	}
 
 	for (i = 0; i < PRUETH_NUM_MACS; i++) {
