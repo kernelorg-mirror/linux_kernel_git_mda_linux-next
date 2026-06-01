c3be7ad9a0ab43b59744ad51b074f5e0fe470c04
MD Danish Anwar <danishanwar@ti.com>
2026-06-01 17:34:43 +0530

    net: ti: icssg-prueth: Fix link-local addresses being forwarded out of slave ports
    Link-local multicast addresses (01:80:c2:00:00:0x) must only be
    delivered to the host port (P0) and must not be forwarded out of
    the physical slave ports. icssg_fdb_add_del() was programming these
    addresses with P1/P2 membership bits set, causing the firmware to
    forward them out of slave ports.
    
    Clear P1/P2 membership and set only P0 membership when
    is_link_local_ether_addr() returns true.
    
    Fixes: 487f7323f39a ("net: ti: icssg-prueth: Add helper functions to configure FDB")
    Signed-off-by: MD Danish Anwar <danishanwar@ti.com>

--- drivers/net/ethernet/ti/icssg/icssg_config.c
+++ drivers/net/ethernet/ti/icssg/icssg_config.c
@@ -736,6 +736,19 @@ int icssg_fdb_add_del(struct prueth_emac *emac, const unsigned char *addr,
 	u8 fid = vid;
 	int ret;
 
+	/* Link-local addresses (01:80:c2:00:00:0x) must only be delivered to
+	 * the host port (P0). Clear P1/P2 membership to prevent the firmware
+	 * from forwarding them out of the physical slave ports.
+	 */
+	if (is_link_local_ether_addr(addr)) {
+		fid_c2 |= ICSSG_FDB_ENTRY_P0_MEMBERSHIP;
+		fid_c2 &= ~(ICSSG_FDB_ENTRY_P1_MEMBERSHIP |
+			    ICSSG_FDB_ENTRY_P2_MEMBERSHIP);
+	}
+
+	netdev_err(emac->ndev, "%s FDB entry for MAC %pM VID %u FID_C2 %u\n",
+		   add ? "Adding" : "Deleting", addr, vid, fid_c2);
+
 	icssg_fdb_setup(emac, &fdb_cmd, addr, fid, add ? ICSS_CMD_ADD_FDB : ICSS_CMD_DEL_FDB);
 
 	fid_c2 |= ICSSG_FDB_ENTRY_VALID;
